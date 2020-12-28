95b154d42bed1a801cb4e423f2fe3fba94d27096
shaikhsahal125 <shaikhsahal125@gmail.com>
2020-12-28 18:15:14 -0500

    UI completed

---
+++ YTB-Download/src/app/Download.java
@@ -0,0 +1,29 @@
+package app;
+
+import controller.FrontPageController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Download extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/frontPage.fxml"));
+
+        AnchorPane root = loader.load();
+//        FrontPageController fpc = loader.getController();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("YouTube Downloader!");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
